25accbbd51aa5a08d4c9a62a22563e6cf5f4e611
Rigo Limon <limon1285@gmail.com>
2021-01-22 23:44:36 -0600

    MVP all endpoints working

--- api/server.js
+++ api/server.js
@@ -1,4 +1,23 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+//server
 const server = express();
+
+//import routers
+const welcomeRouter = require("../welcome/welcome-router");
+const actionRouter = require("./actions/actions-router");
+const projectRouter = require("./projects/projects-router");
+
+//Global middleware
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+//Server endpoints --------->
+server.use("/", welcomeRouter);
+server.use("/api/actions", actionRouter);
+server.use("/api/projects", projectRouter);
 
 module.exports = server;
