ecafeb0cee17e59c30396ec0af3567c73dc8c9c2
Rigo <limon1285@gmail.com>
2021-01-22 21:36:08 -0600

    added server, codeGrade test

--- api/server.js
+++ api/server.js
@@ -1,7 +1,4 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
 
 module.exports = server;
