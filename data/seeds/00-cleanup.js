a88851263f1a756245a902d92a5b1cbb16810187
Rigo <limon1285@gmail.com>
2021-01-24 15:00:11 -0600

    stretch done

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,6 +1,6 @@
-const { clean } = require("knex-cleaner");
+const clean = require("knex-cleaner");
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   return clean(knex, {
     mode: "truncate",
     ignoreTables: ["knex_migrations", "knex_migrations_lock"],
